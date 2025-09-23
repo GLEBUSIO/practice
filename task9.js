c7d0e7bb6ea260494029fbbf1c240aec01778c07
Gleb Avlasevich <g.avlasevich@mail.ru>
2025-09-23 13:38:32 +0300

    smth

---
+++ task9.js
@@ -0,0 +1,19 @@
+async function f( ) {
+
+let githubResponse = await fetch(`https://api.github.com/users/GLEBUSIO`);
+let githubUser = await githubResponse.json();
+
+ let img = document.createElement('img');
+  img.src = githubUser.avatar_url;
+  img.className = "promise-avatar-example";
+  document.body.append(img);
+
+  
+  await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+
+  img.remove();
+
+  return githubUser;
+}
+
+f();
